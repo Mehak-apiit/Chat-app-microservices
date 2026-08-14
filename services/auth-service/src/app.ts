56aa01a6c6ca7d43e1378ccaa41d9581d2970c45
Mehak <mehak.apiit@gmail.com>
2026-08-14 20:27:29 +0530

    feat: setup shared utils

---
+++ services/auth-service/src/app.ts
@@ -0,0 +1,5 @@
+import express, {type Application} from "express";
+export const createApp = (): Application => {
+    const app = express();
+    return app;
+};
---
+++ services/auth-service/src/utils/looger.ts
@@ -0,0 +1,3 @@
+import {createLogger} from "@chatapp/common";
+import type {Logger} from "@chatapp/common";
+export const logger: Logger = createLogger({name: "auth-service"});
