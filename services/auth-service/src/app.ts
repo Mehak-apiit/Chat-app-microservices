720ab1fe1c2a97381dd6d4c192d5bdb5f223f356
Mehak <mehak.apiit@gmail.com>
2026-08-18 20:38:16 +0530

    feat(auth): setup auth service

--- services/auth-service/src/app.ts
+++ services/auth-service/src/app.ts
@@ -1,5 +1,22 @@
 import express, {type Application} from "express";
+import cors from "cors" ; 
+import helmet from "helmet";
+import {errorHandler} from "@/middleware/error-handler";
 export const createApp = (): Application => {
     const app = express();
-    return app;
+    app.use(helmet());//***************** */
+    app.use(cors({
+        origin:"*",
+        credentials:true,//*************/
+    })
+);
+
+
+app.use(express.json());//*********** */
+app.use(express.urlencoded({ extended: true }));//************** */
+app.use((_req,res)=>{
+    res.status(404).json({message:"Not Found"});
+});
+app.use(errorHandler);
+return app;
 };
